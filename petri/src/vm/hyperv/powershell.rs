df5a1b6bedf5a4fa54583a4ee12598be787a54cd
Steven Malis <137308034+smalis-msft@users.noreply.github.com>
2026-09-25 17:34:26 -0400

    petri: Enable setting TPM version for hyperv + openhcl
    Now that ARM UEFI fixes are ingested this should pass.

--- petri/src/vm/hyperv/powershell.rs
+++ petri/src/vm/hyperv/powershell.rs
@@ -246,19 +246,19 @@ pub struct HyperVManagementVtlFeatureFlags {
     pub _reserved2: u64,
 }
 
-// impl HyperVManagementVtlFeatureFlags {
-//     fn with_tpm_version(self, version: Option<crate::PetriTpmVersion>) -> Self {
-//         match version {
-//             Some(crate::PetriTpmVersion::V138) => self
-//                 .with_use_tpm_138_by_default(true)
-//                 .with_use_tpm_185_by_default(false),
-//             Some(crate::PetriTpmVersion::V185) => self
-//                 .with_use_tpm_138_by_default(false)
-//                 .with_use_tpm_185_by_default(true),
-//             None => self,
-//         }
-//     }
-// }
+impl HyperVManagementVtlFeatureFlags {
+    fn with_tpm_version(self, version: Option<crate::PetriTpmVersion>) -> Self {
+        match version {
+            Some(crate::PetriTpmVersion::V138) => self
+                .with_use_tpm_138_by_default(true)
+                .with_use_tpm_185_by_default(false),
+            Some(crate::PetriTpmVersion::V185) => self
+                .with_use_tpm_138_by_default(false)
+                .with_use_tpm_185_by_default(true),
+            None => self,
+        }
+    }
+}
 
 impl ps::AsVal for HyperVManagementVtlFeatureFlags {
     fn as_val(&self) -> impl '_ + AsRef<OsStr> {
@@ -567,8 +567,7 @@ impl HyperVNewCustomVMArgs {
                         .map(|p| p.is_strict())
                         .unwrap_or(false),
                 )
-                // TODO Once UEFI is fixed
-                //.with_tpm_version(tpm.as_ref().map(|t| t.version))
+                .with_tpm_version(tpm.as_ref().map(|t| t.version))
             }),
             guest_state_encryption_policy: {
                 // A requested hardware sealing policy takes precedence over the
